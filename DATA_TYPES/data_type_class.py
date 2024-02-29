2c2cee0033d09501cc0963456687ec822008c19e
Mr Muluhya Lisiolo <221100@AD.UONBI.AC.KE>
2024-02-29 12:58:51 +0300

    first commit

---
+++ DATA_TYPES/data_type_class.py
@@ -0,0 +1,28 @@
+a=100
+b=3.14
+#print(a*b)
+c=5+7j
+#print(a+b-c)
+d=False
+#print(d)
+str="Welcome to DATA TYPES"
+#print(str)
+#print(str[-1])
+#print(str[4:8])
+#plp=" python class"
+#print(str+plp)
+#print(plp*3)
+#Shopping_list=['bread','milk','eggs', '50kg sugar','3lt cooking oil',20, 3+5d, 300]
+#Print(Shopping_list)
+#Building_List=[450, 'wheelbarrows', 320,'more', "bread",'milk']
+#print(Building_List)
+a=10
+b=3
+#print(a%b)
+x={}
+x[2]=10
+x[1]=[20,30,40]
+#print(x[1][2])
+input("Cool")
+name=Testing
+print(len(name))
---
+++ user_input.py
@@ -0,0 +1,10 @@
+# user_input.py
+# Asking for user's name
+name = "What is your name?_____________________ "
+# Asking for user's age
+age = "(Indicate Age)__________ "
+# Asking for user's location
+location = "(Indicate Location)_______________"
+# Printing personalized message
+print("Hello. {}, you are {} years old and live in {}.".format(name, age, location))
+
